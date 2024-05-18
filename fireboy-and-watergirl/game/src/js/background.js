648a4dcf9dcaae41540b58beda219d5c1305c0c0
abbbaf <abbbaf@gmail.com>
2024-05-18 14:58:53 +0300

    Add files via upload

---
+++ fireboy-and-watergirl/game/src/js/background.js
@@ -0,0 +1,5 @@
+let background = new Image();
+background.src = "/fireboy-and-watergirl/spritesheet/background.svg";
+
+let timerImage = new Image();
+timerImage.src = "/fireboy-and-watergirl/spritesheet/text/timer.png";
