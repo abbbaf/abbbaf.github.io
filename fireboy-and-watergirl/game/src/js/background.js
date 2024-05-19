3bdbd635d6bdbe1fc9ce197e8144185a41f459b6
abbbaf <abbbaf@gmail.com>
2024-05-19 19:27:19 +0300

    Delete fireboy-and-watergirl directory

--- fireboy-and-watergirl/game/src/js/background.js
+++
@@ -1,5 +0,0 @@
-let background = new Image();
-background.src = "/fireboy-and-watergirl/spritesheet/background.svg";
-
-let timerImage = new Image();
-timerImage.src = "/fireboy-and-watergirl/spritesheet/text/timer.png";
