4b4146c6789a221d5c13ef87fbe84b4233f096e8
abbbaf <abbbaf@gmail.com>
2026-03-14 14:29:10 +0200

    Delete functions.m

--- functions.m
+++
@@ -1,7 +0,0 @@
-let GetFunctions = () => [
-        GenerateSalaryTable = GetFunction("https://abbbaf.github.io/hours.m"),
-        WorkerDataSummary = GetFunction("https://abbbaf.github.io/data_summary.m"),
-        GetShabatAndHolidayTable = GetFunction("https://abbbaf.github.io/shabat_hours.m")
-    ]
-in
-    etFunctions
