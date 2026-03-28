39dde149938de5d13f13ad92b5455e9dfe87d3c8
abbbaf <abbbaf@gmail.com>
2026-03-28 14:12:50 +0300

    Update fetch_data.m

--- fetch_data.m
+++ fetch_data.m
@@ -1,5 +1,5 @@
 let 
-    sha256 = "671705c51b2e42b1badfd3ca44d699f1364b2ba61fd500f16f45de8357ddc0f5",
+    sha256 = "db4da2a73a9d299fd2cb32a475609dfc7cbd9569e1fec4223de56b3962d09a2f",
     url = "http://abbbaf.github.io/functions.m",
     expression = Text.FromBinary(Web.Contents(url,[IsRetry=true])),   
     sha256_url = "https://api.hashify.net/hash/sha256/hex?value=" &  Uri.EscapeDataString(expression),
